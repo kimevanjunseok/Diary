4b120fd9c2bacc02c190b4f9bdd4a7c02a06be70
kimevanjunseok <jnunseok@gmail.com>
2019-11-21 17:43:23 +0900

    MainPage UI grid 작업

--- frontend/src/views/MainPage.vue
+++ frontend/src/views/MainPage.vue
@@ -1,3 +1,36 @@
 <template>
-    <h1>이곳은 MAINPAGE</h1>
+    <div class="row">
+        <div class="col-4 array">
+            test
+        </div>
+        <div class="col-4 array">
+            test
+        </div>
+        <div class="col-4 array">
+            test
+        </div>
+        <div class="col-4 array">
+            test
+        </div>
+    </div>
 </template>
+
+<script>
+export default {
+    name: "MainPage",
+    data() {
+        return {
+            postdata: []
+        }
+    },
+}
+</script>
+
+<style scoped>
+.array {
+    flex: 31% !important;
+    max-width: 31% !important;
+    border: 1px black solid;
+    margin: 10px;
+}
+</style>
