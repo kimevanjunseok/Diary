29886fd2d94eb898ae03f472f19886dd32a22f47
kimjunseok <jnunseok@gmail.com>
2019-12-04 12:39:26 +0900

    feat: User serializers.py 생성

--- backend/auth/serializers.py
+++ backend/auth/serializers.py
@@ -1,4 +1,35 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate
 
+User = get_user_model()
+
+# 회원가입 시리얼라이저
+class CreateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "username", "password")
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            validated_data["username"], None, validated_data["password"]
+        )
+        return user
+
+# 로그인 시리얼라이저
+class LoginUserSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Unable to log in with provided credentials.")
+    
+# 접속 유지중인지 확인 시리얼라이저
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "username")
